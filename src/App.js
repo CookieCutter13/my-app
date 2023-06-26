f7f9ebcf838203025795966a6accc4f7866285c7
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-06-26 15:42:40 -0700

    css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <h1>Weather App</h1>
       <footer>
         THIS PROJECT WAS CODED BY DESIRÉE G AND IS OPEN-SOURCED
-        Weather App
         <a href="https://github.com/CookieCutter13/my-app" target="blank"></a>
       </footer>
     </div>
