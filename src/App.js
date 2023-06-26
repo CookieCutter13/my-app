93e72d5ae025848b915fe1c0aa1a34c5cd033932
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-06-26 15:40:06 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <footer>
+        THIS PROJECT WAS CODED BY DESIRÉE G AND IS OPEN-SOURCED
+        Weather App
+        <a href="https://github.com/CookieCutter13/my-app" target="blank"></a>
+      </footer>
+    </div>
+  );
 }
