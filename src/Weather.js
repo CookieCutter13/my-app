ff2f2d2d7a754f1453c5abff0576c3d1194ea325
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-06-26 18:24:18 -0700

    define

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,44 @@
 import React from "react";
-import "./Weather.css"
+import "./Weather.css";
 
-export default function Weather(){
-  return <div>Hello</div>
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />{" "}
+          </div>
+        </div>
+      </form>
+      <h2>New York</h2>
+      <ul>
+        <li>Wednesday 7</li>
+        <li>mostly stupid</li>{" "}
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="mostly cloudy"
+          />
+          6ºC
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>precip: 15%</li>
+            <li>humid: a lot</li>
+            <li>wind:100mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
