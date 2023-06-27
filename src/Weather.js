11e12ef1937abc4cc2e41b4079ef606644a00d72
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-06-26 19:36:02 -0700

    add function

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  const apiKey = "27o0abe982dat860dde4ff04f45c83ea";
-  let city = "New York"
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}}&units=imperial`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a City........"
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />{" "}
-          </div>
-        </div>
-      </form>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.main.wind.speed,
+      city: response.data.name,
+    });
 
-      <h2>New York </h2>
-      <ul>
-        <li>Wednesday 7</li>
-        <li>mostly stupid</li>{" "}
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="mostly cloudy"
-          />
-          <strong>6º</strong> C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>precip: 15%</li>
-            <li>humid: a lot</li>
-            <li>wind:100mph</li>
-          </ul>
+    setTemperature();
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a City........"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />{" "}
+            </div>
+          </div>
+        </form>
+
+        <h2>New York </h2>
+        <ul>
+          <li>Wednesday 7</li>
+          <li>mostly stupid</li>{" "}
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly cloudy"
+            />
+            <div className="float-left">
+              <span className="temperature">{Math.round(temperature)}</span>
+              <span className="unit">ºF</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>precip: 15%</li>
+              <li>humid: a lot</li>
+              <li>wind:100mph</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "27o0abe982dat860dde4ff04f45c83ea";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
