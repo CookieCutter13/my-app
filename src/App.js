5dd682f197b4be6a3ba59eb0340cc40388a0ad72
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-06-28 13:44:02 -0700

    remove

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather.js";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
       <Weather defaultCity="New York" />
+    
       <footer>
         THIS PROJECT WAS CODED BY DESIRÉE G AND IS
         <a href="https://github.com/CookieCutter13/my-app" target="blank">
