30e7b29bab3b944e9f77ac705af8c3383e39dc3d
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-07-03 14:18:57 -0700

    add weather

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,11 +16,11 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <div className="float-left">
+            <div className="d-flex">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
+              <WeatherTemperature  />
             </div>
           </div>
         </div>
